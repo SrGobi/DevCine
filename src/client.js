8b3a45a91c0a3745a564e4e87255986d1eec53eb
SrGobi <alejandrosanjuangallegos@gmail.com>
2023-06-06 09:58:26 +0200

    first commit

---
+++ src/client.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", async () => {
+  const moviesContainer = document.getElementById("movies-container");
+
+  // Obtener películas del servidor
+  const response = await fetch(`/movies`);
+  const movies = await response.json();
+
+  // Mostrar las películas en el contenedor
+  movies.forEach((movie) => {
+    const movieCard = createMovieCard(movie);
+    moviesContainer.appendChild(movieCard);
+  });
+});
+
+function createMovieCard(movie) {
+  const movieCard = document.createElement("div");
+  movieCard.classList.add("movie-card");
+
+  const image = document.createElement("img");
+  image.src = movie.img;
+  image.alt = movie.title;
+  movieCard.appendChild(image);
+
+  const title = document.createElement("h2");
+  title.textContent = movie.title;
+  movieCard.appendChild(title);
+
+  const year = document.createElement("p");
+  year.textContent = `Year: ${movie.year}`;
+  movieCard.appendChild(year);
+
+  const genre = document.createElement("p");
+  genre.textContent = `Genre: ${movie.genre}`;
+  movieCard.appendChild(genre);
+
+  // Agregar un enlace al detalle de la película
+  const detailsLink = document.createElement("a");
+  detailsLink.href = `src/movie/movie.html?id=${movie._id}`;
+  detailsLink.textContent = "Ver detalles";
+  movieCard.appendChild(detailsLink);
+
+  return movieCard;
+}
---
+++ src/movie/movie.js
@@ -0,0 +1,77 @@
+document.addEventListener("DOMContentLoaded", async () => {
+  // Obtener el ID de la película de la URL actual
+  const urlParams = new URLSearchParams(window.location.search);
+  const movieId = urlParams.get("id");
+
+  // Verificar si se proporcionó un ID de película válido
+  if (movieId) {
+    // Obtener los detalles de la película desde el servidor
+    const response = await fetch(`/movie/${movieId}`);
+    const movie = await response.json();
+
+    // Mostrar los detalles de la película en el contenedor
+    const movieDetailsContainer = document.getElementById("movie-details-container");
+    movieDetailsContainer.appendChild(createMovieDetailsHTML(movie));
+  } else {
+    // Mostrar un botón de prueba para volver a la página principal
+    const backButtonContainer = document.getElementById("back-button-container");
+
+    const backButton = document.createElement("button");
+    backButton.textContent = "Back to Home";
+    backButton.addEventListener("click", () => {
+      window.location.href = "index.html";
+    });
+
+    backButtonContainer.appendChild(backButton);
+  }
+});
+
+function createMovieDetailsHTML(movie) {
+  const movieDetails = document.createElement("div");
+  movieDetails.classList.add("movie-details");
+
+  const backButton = document.createElement("button");
+  backButton.textContent = "Back to Home";
+  backButton.addEventListener("click", () => {
+    window.location.href = "index.html";
+  });
+  movieDetails.appendChild(backButton);
+
+  const image = document.createElement("img");
+  image.src = movie.img;
+  image.alt = movie.title;
+  movieDetails.appendChild(image);
+
+  const title = document.createElement("h2");
+  title.textContent = movie.title;
+  movieDetails.appendChild(title);
+
+  const year = document.createElement("p");
+  year.textContent = `Year: ${movie.year}`;
+  movieDetails.appendChild(year);
+
+  const genre = document.createElement("p");
+  genre.textContent = `Genre: ${movie.genre}`;
+  movieDetails.appendChild(genre);
+
+  const linksContainer = document.createElement("div");
+  linksContainer.classList.add("links-container");
+
+  movie.links.forEach((link) => {
+    const torrentLink = document.createElement("a");
+    torrentLink.href = link;
+    torrentLink.target = "_blank";
+    torrentLink.classList.add("torrent-link");
+
+    const button = document.createElement("button");
+    button.classList.add("torrent-button");
+    button.textContent = "TORRENT";
+
+    torrentLink.appendChild(button);
+    linksContainer.appendChild(torrentLink);
+  });
+
+  movieDetails.appendChild(linksContainer);
+
+  return movieDetails;
+}
